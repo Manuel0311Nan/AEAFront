a114e5df32c2cd03d98ca306da326201dcb0cf47
Manuel Alcázar <manuzz3112@gmail.com>
2022-08-19 17:41:50 +0200

    añadimos google.maps a terminales

--- src/pages/Terminales/Detail/Detail.jsx
+++ src/pages/Terminales/Detail/Detail.jsx
@@ -21,29 +21,31 @@ const Detail = () => {
       ) : (
         <>
           <section className="col container">
-              <div className="card m-2 bg-primary d-flex flex-column align-items-center">
+            <div className="card m-2 bg-primary d-flex flex-column align-items-center">
               <img
-                  className="img-fluid w-25"
-                  src={terminalesDetail.logo}
-                  alt={terminalesDetail.logo}
-                />
+                className="img-fluid w-25"
+                src={terminalesDetail.logo}
+                alt={terminalesDetail.logo}
+              />
               <div className="card-body ">
                 <h3 className="text-center text-light">
                   {terminalesDetail.name}
                 </h3>
-                 <a href="https://www.google.es/maps/dir/WFS+Servicios+Aeroportuarios+-++HDQ%E2%80%A6/PR+Sur,+Av.+Central,+61,+28042+Madrid/@40.4536317,-3.5737051,16.17z/data=!3m1!5s0xd423033ac9eea09:0x52074854acf8b775!4m14!4m13!1m5!1m1!1s0xd4231dcef01886b:0x3c3b0fb82e65c6be!2m2!1d-3.5780339!2d40.4548982!1m5!1m1!1s0xd4230338504f411:0x1c9db5b16f2d2273!2m2!1d-3.5666483!2d40.4550383!3e0?hl=es"><img
-                  className="img-fluid "
-                  src={terminalesDetail.image}
-                  alt={terminalesDetail.name}
-                /></a> 
+                <a href={terminalesDetail.destiny}>
+                  <img
+                    className="img-fluid "
+                    src={terminalesDetail.image}
+                    alt={terminalesDetail.name}
+                  />
+                </a>
                 <h3 className="text-light text-center">
                   {" "}
                   Entrada por: {terminalesDetail.acceso}
-                  </h3>
-                  {terminalesDetail.doc.filter((info) =>
-                    <h3 className="text-light mt-2 text-wrap">
-                   {info[0]}
-                </h3>)}
+                </h3>
+                {/* {terminalesDetail.doc.filter((info) => */}
+                <h3 className="text-light mt-2 text-wrap">
+                  {terminalesDetail.doc}
+                </h3>
               </div>
             </div>
           </section>
