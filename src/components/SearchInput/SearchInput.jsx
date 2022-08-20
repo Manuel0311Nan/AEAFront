612e70f716a3ca9741e9ec1098c6b9933175fb2e
Manuel Alcázar <manuzz3112@gmail.com>
2022-08-20 13:52:13 +0200

    desarollo terminado

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const SearchInput = ({...rest}) => {
+  return (
+    <div className="">
+
+      <input {...rest} className="m-2" />
+
+    </div>
+  )
+}
--- src/pages/Almacen/Almacen.jsx
+++ src/pages/Almacen/Almacen.jsx
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from "react";
 // import { Link } from "react-router-dom";
 import { BASE_URL } from "../../assets/apiRoutes";
+import { SearchInput } from "../../components/SearchInput/SearchInput";
+// import { Ubicaciones } from "./Ubicaciones";
 const Almacen = () => {
   let [ubicaciones, setUbicaciones] = useState([]);
+
+  const [keyword, setKeyword] = useState("");
 
   useEffect(() => {
     fetch(`${BASE_URL}/almacen/`)
       .then((response) => response.json())
       .then((data) => setUbicaciones(data));
   }, []);
-
+  const onInputChange = (e) => {
+    setKeyword(e.target.value.toLowerCase());
+  };
+  const filteredUbicaciones = ubicaciones.filter(ubicacion => ubicacion.ubicacion.toLowerCase().includes(keyword));
   return (
     <main>
       <section className="py-5 text-center container">
@@ -22,26 +29,30 @@ const Almacen = () => {
           </div>
         </div>
       </section>
-      <section className="container ">
-        {ubicaciones.map((ubicacion, key) => (
-          <table key={ubicacion._id} class="display table table-striped text-center mx-1">
-            <thead>
-              <tr>
-                <th className="col-3">Ubicacion</th>
-                <th className="col-3">Planta</th>
-                <th className="col-3">Nave</th>
+      <>
+      <SearchInput placeholder="Filtra por Ubicacion " onChange={onInputChange} />
+        <section className="container">
+        <table  class="display table table-striped text-center mx-1">
+        <thead>
+          <tr>
+            <th className="col-2">Ubicacion</th>
+            <th className="col-2">Planta</th>
+            <th className="col-2">Nave</th>
+          </tr>
+        </thead>
+        {filteredUbicaciones.map(ubicacion => (
+        <tbody key={ubicacion._id}>
+          <tr>
+            <td className="text-start text-md-center">{ubicacion.ubicacion}</td>
+            <td>{ubicacion.planta}</td>
+            <td>{ubicacion.nave}</td>
               </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>{ubicacion.ubicacion}</td>
-                <td>{ubicacion.planta}</td>
-                <td>{ubicacion.nave}</td>
-              </tr>
-            </tbody>
-          </table>
-        ))}
-      </section>
+          </tbody>
+          ))}
+      </table>
+
+  </section>
+    </>
     </main>
   );
 };
