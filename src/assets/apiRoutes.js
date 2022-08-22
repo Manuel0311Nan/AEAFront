e38a1ab67380b7093d4aa25b2d89b6bd5656304d
Manuel Alcázar <manuzz3112@gmail.com>
2022-08-22 18:22:42 +0200

    modificando enlace a heroku

--- src/assets/apiRoutes.js
+++ src/assets/apiRoutes.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://aeaback.herokuapp.com";
-//export const BASE_URL = "http://localhost:4000"
+//export const BASE_URL = "https://aeaback.herokuapp.com";
+export const BASE_URL = "http://localhost:4000"
